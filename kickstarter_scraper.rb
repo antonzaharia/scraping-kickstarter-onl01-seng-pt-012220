bb6779c45df6953d4823a44b4efcf9a7bab4de3b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-16 10:41:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("ul.project-meta").text
-  # funded: project.css("ul.project-stats")
+  # funded: project.css("ul.project-stats li.first.funded strong").text")
 end
 
 create_project_hash
